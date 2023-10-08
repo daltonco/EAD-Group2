0526eac9aba6a5092aaae77cc1f55b4983e693f8
colton <daltonco@mail.uc.edu>
2023-10-08 17:28:13 -0400

    Update AdoptMePlusController.java
    added adoption get, post, & delete mappings. tested with postman. will need to do the same for dog & customer.

--- src/main/java/com/adoptmeplus/enterprise/AdoptMePlusController.java
+++ src/main/java/com/adoptmeplus/enterprise/AdoptMePlusController.java
@@ -1,13 +1,37 @@
 package com.adoptmeplus.enterprise;
 
+import com.adoptmeplus.enterprise.dto.Adoption;
+import com.adoptmeplus.enterprise.service.IAdoptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AdoptMePlusController {
+
+    @Autowired
+    IAdoptionService adoptionService;
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+    @GetMapping("/")
+    public ResponseEntity fetchAllAdoptions(){
+        return new ResponseEntity(HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity fetchAdoptionsById(@PathVariable("adoptionID") String id){
+        return new ResponseEntity(HttpStatus.OK);
+    }
+    @PostMapping(value="/", consumes="application/json", produces="application/json")
+    public Adoption createAdoption(@RequestBody Adoption adoption){
+        return adoption;
+    }
+    @DeleteMapping("/{id}/")
+    public ResponseEntity deleteAdoption(@PathVariable("adoptionID") String id){
+        return new ResponseEntity(HttpStatus.OK);
     }
     @GetMapping("/dogs")
     public String dogs() {
