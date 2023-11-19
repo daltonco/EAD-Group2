257316a2242727f5754a8a62bf0660c28b49e4aa
thomasza92 <127898797+thomasza92@users.noreply.github.com>
2023-11-19 17:27:46 -0500

    customers mapping fixed typo & CircleCI fixed?

---
+++ src/main/java/com/adoptmeplus/enterprise/dao/AdoptionDAOStub.java
@@ -0,0 +1,67 @@
+package com.adoptmeplus.enterprise.dao;
+
+import com.adoptmeplus.enterprise.dto.Adoption;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * The AdoptionDAO class is responsible for managing and interacting with the database or data source
+ * for the Adoption entities in the AdoptMePlus application.
+ *
+ * This class implements the IAdoptionDAO interface and provides methods for saving and retrieving Adoption records.
+ *
+ * @author AdoptMePlusDevTeam
+ * @version 1.0
+ */
+@Repository("adoptionDAO")
+public class AdoptionDAOStub implements IAdoptionDAO{
+
+    /**
+     * Saves an Adoption record to the data source.
+     *
+     * @param adoption The Adoption object to be saved.
+     * @return The saved Adoption object.
+     */
+    @Override
+    public Adoption save (Adoption adoption) {
+        return null;
+    }
+
+    /**
+     * Retrieves a list of all Adoption records from the data source.
+     *
+     * @return A List of Adoption objects representing all available Adoption records.
+     */
+    @Override
+    public List<Adoption> findAll(){
+        return null;
+    }
+
+    /**
+     * Deletes an adoption record from the data source.
+     *
+     * @param adoption The Adoption object to be deleted.
+     * @return The delete Adoption object.
+     */
+
+    @Override
+    public void delete(Adoption adoption){
+        return;
+    }
+
+    /**
+     * Fetches a Adoption by its unique identifier.
+     *
+     * @param adoptionId The unique identifier of the adoption to be fetched.
+     * @return The adoption object with the specified adoptionId, or null if not found.
+     * @throws IOException if there's an issue with the network communication.
+     */
+    @Override
+    public Adoption fetchAdoption(int adoptionId) throws IOException {
+        return null;
+    }
+}
