18298b4794adb0043d88fbe85edb65cc8a0f4219
patelsheel <patel7sa@mail.uc.edu>
2023-11-23 19:35:06 -0500

    Commit 1- Modified RetrofitClientInstance
    Implemented changes include a constant base URL for easy future modifications, lazy initialization of Retrofit for optimized resource use, and a private constructor to prevent accidental class instantiation. These enhancements improve code flexibility, efficiency, and maintainability.

--- src/main/java/com/adoptmeplus/enterprise/dao/RetrofitClientInstance.java
+++ src/main/java/com/adoptmeplus/enterprise/dao/RetrofitClientInstance.java
@@ -7,19 +7,28 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstance {
 
+    private static final String BASE_URL = "http://localhost:8080";
     private static Retrofit retrofit;
 
+    private RetrofitClientInstance() {
+        // Private constructor to prevent instantiation
+    }
+
     public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = createRetrofitInstance();
+        }
+        return retrofit;
+    }
+
+    private static Retrofit createRetrofitInstance() {
         Gson gson = new GsonBuilder()
                 .setLenient()
                 .create();
-        if (retrofit == null) {
-            String BASE_URL = "http://localhost:8080";
-            retrofit = new retrofit2.Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
-        }
-        return retrofit;
+
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
     }
 }
